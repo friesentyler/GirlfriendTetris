8e352698e56300035b0f74009b2bb096b79f1876
Tyler <tfriesen2@my.gcu.edu>
2025-04-04 10:32:48 -0700

    added ability to blit board to the screen, some of the game logic is still broken

--- Assets/index.js
+++ Assets/index.js
@@ -11,29 +11,55 @@ function main() {
 	userGame.addPieceToBoard();
 	// DELETE ME
 	userGame.gameBoard[19] = [1, 1, 1, 1, 1, 1, 0, 1, 1, 1];
+	blitScreen(userGame.gameBoard);
 	let reset = document.querySelector('.reset-button');
 	reset.addEventListener('click', (event) => {
+		userGame.generatePieceOutline();
 		userGame.exertGravityOnBoard();
 		userGame.clearLines();
+		blitScreen(userGame.gameBoard);
 		console.log(userGame.gameBoard);
 	});
 	document.addEventListener('keydown', (event) => {
 		console.log(event.code);
 		if (event.code === "ArrowLeft") {
+			event.preventDefault();
 			userGame.movePieceLeft();
 			userGame.generatePieceOutline();
+			blitScreen(userGame.gameBoard);
 		} else if (event.code === "ArrowRight") {
+			event.preventDefault();
 			userGame.movePieceRight();
 			userGame.generatePieceOutline();
+			blitScreen(userGame.gameBoard);
 		} else if (event.code === "Space") {
+			event.preventDefault();
 			userGame.slamPiece();
 			userGame.generatePieceOutline();
+			blitScreen(userGame.gameBoard);
 		} else if (event.code === "ArrowUp") {
+			event.preventDefault();
 			userGame.rotatePiece();
 			userGame.generatePieceOutline();
+			blitScreen(userGame.gameBoard);
 		}
 		console.log(userGame.gameBoard);
 	});
+}
+
+function blitScreen(board) {
+	for (let i = 0; i < board.length; i++) {
+		for (let j = 0; j < board[0].length; j++) {
+			let block = document.getElementById(`${i}-${j}`);
+			if (board[i][j] === 1) {
+				block.style.backgroundColor = 'hsl(0, 100%, 50%)';
+			} else if (board[i][j] === -1) {
+				block.style.backgroundColor = 'hsl(0, 100%, 5%)';
+			} else {
+				block.style.removeProperty('background-color');
+			}
+		}
+	}
 }
 
 // first off I should make something that represents just the tetris logic by itself
