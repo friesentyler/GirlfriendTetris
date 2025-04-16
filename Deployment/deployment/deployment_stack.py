48ec31c5cf4906a701845ed4573a5156ddc03506
Tyler <tfriesen2@my.gcu.edu>
2025-04-15 20:29:50 -0700

    added default route to the application and request throttling on the API Gateway

--- Deployment/deployment/deployment_stack.py
+++ Deployment/deployment/deployment_stack.py
@@ -1,5 +1,4 @@
 from aws_cdk import (
-    # Duration,
     Stack,
     aws_lambda as _lambda,
     CfnOutput,
@@ -7,18 +6,17 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_s3_deployment as s3deploy,
     aws_iam as iam
-    # aws_sqs as sqs,
 )
 import aws_cdk as cdk
 from constructs import Construct
-#from aws_cdk.aws_apigatewayv2_integrations import HttpLambdaIntegration
-#from aws_cdk.aws_apigatewayv2_integrations import HttpUrlIntegration
 
 class DeploymentStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # This gives the API Gateway the role necessary to access the S3 public
+        # this way we don't need to make the S3 bucket public
         role = iam.Role(
             self, "ApiGatewayS3Role",
             assumed_by=iam.ServicePrincipal("apigateway.amazonaws.com"),
@@ -50,27 +48,19 @@ class DeploymentStack(Stack):
             destination_bucket=bucket,
         )
 
+        # defines the rest api, API Gateway only accepts the first type in the Accept header, which is why we have
+        # non binary media types listed here. This allows us to properly serve our images and text content
         rest_api = apigateway.RestApi(self, "game-api",
-            binary_media_types=["image/png", "text/css", "application/javascript", "text/html", "image/*", "application/octet-stream", "application/json"]
+            binary_media_types=["image/png", "text/css", "application/javascript", "text/html", "image/*", "application/octet-stream", "application/json"],
+            deploy_options=apigateway.StageOptions(throttling_burst_limit=8, throttling_rate_limit=1)
         )
-        #resource = rest_api.root.add_resource("{proxy+}")
-        #resource = rest_api.root.add_resource("index.html")
-        '''resource.add_method("GET", apigateway.AwsIntegration(
-            service="s3",
-            path=f"{bucket.bucket_name}/index.html",
-            integration_http_method="GET",
-            options=apigateway.IntegrationOptions(
-                credentials_role=role
-            ),
-            proxy=True
-        ))'''
-
         proxy_resource = rest_api.root.add_resource("{proxy+}")
 
-        proxy_resource.add_method("GET", apigateway.AwsIntegration(
+        # sets up the proxy method, so we can grab any of the resources from the S3 bucket
+        proxy_route = proxy_resource.add_method("GET", apigateway.AwsIntegration(
             service="s3",
             path=f"{bucket.bucket_name}/{{proxy}}",
-            region="us-west-2",  # your region here
+            region="us-west-2",
             integration_http_method="GET",
             options=apigateway.IntegrationOptions(
                 credentials_role=role,
@@ -110,31 +100,50 @@ class DeploymentStack(Stack):
             apigateway.MethodResponse(status_code="500"),
         ])
 
+        # maps the default route of the application to index.html
+        default_route = rest_api.root.add_method("GET", apigateway.AwsIntegration(
+            service="s3",
+            path=f"{bucket.bucket_name}/index.html",
+            region="us-west-2",
+            integration_http_method="GET",
+            options=apigateway.IntegrationOptions(
+                credentials_role=role,
+                request_parameters={
+                    "integration.request.path.proxy": "method.request.path.proxy"
+                },
+                integration_responses=[
+                    apigateway.IntegrationResponse(
+                        status_code="200",
+                        selection_pattern="",
+                        response_parameters={
+                            "method.response.header.Content-Type": "integration.response.header.Content-Type"
+                        }
+                    ),
+                    apigateway.IntegrationResponse(
+                        status_code="400",
+                        selection_pattern="4\\d{2}",
+                        response_templates={"application/json": '{"error": "Client Error"}'}
+                    ),
+                    apigateway.IntegrationResponse(
+                        status_code="500",
+                        selection_pattern="5\\d{2}",
+                        response_templates={"application/json": '{"error": "Server Error"}'}
+                    )
+                ]
+            )
+        ), request_parameters={
+            "method.request.path.proxy": True
+        }, method_responses=[
+            apigateway.MethodResponse(
+                status_code="200",
+                response_parameters={
+                    "method.response.header.Content-Type": True
+                }
+            ),
+            apigateway.MethodResponse(status_code="400"),
+            apigateway.MethodResponse(status_code="500"),
+        ])
 
-
-        '''rest_api.add_usage_plan("UsagePlan",
-            name="default",
-            throttle=apigateway.ThrottleSettings(rate_limit=10, burst_limit=2)
-        )'''
-
-        '''get_gamefiles = HttpUrlIntegration("gamefilesStaticIntegration", bucket.virtual_hosted_url_for_object(key="index.html"))
-        book_store_default_integration = HttpLambdaIntegration("BooksIntegration", my_function)
-
-        http_api = apigwv2.HttpApi(self, "HttpApi", 
-            default_integration=HttpUrlIntegration("DefaultIntegration", "https://example.com")
-        )
-
-        http_api.add_routes(
-            path="/",
-            methods=[apigwv2.HttpMethod.GET],
-            integration=get_gamefiles
-        )
-
-        http_api.add_routes(
-            path="/books",
-            methods=[apigwv2.HttpMethod.ANY],
-            integration=book_store_default_integration
-        )'''
 
         '''my_function = _lambda.Function(
             self, "HelloWorldFunction",
